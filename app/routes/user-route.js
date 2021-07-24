d2fdc4de23d8a182d2f067387cdf6140baadb83b
Swapnali <swapnalisuntnure11@gmail.com>
2021-07-24 22:16:20 +0530

    user route

--- app/routes/user-route.js
+++ app/routes/user-route.js
@@ -1,16 +1,20 @@
-module.exports = app => {
-    const user = require("../controllers/user-controller.js");
-  
+//module.exports = app => {
+    const user = require("../controllers/user-controller");
+    const userLogin = require("../controllers/loginController");
+    const validateToken  = require("../controllers/helpers/index").validateToken;
     var router = require("express").Router();
-  
-    // Create a new Tutorial
+    module.exports = app => {
+    // Create a new user
     router.post("/", user.create);
+    //login user
+    router.post("/login", userLogin.login);
+
+    router.post("/register", userLogin.register);
 
     router.post("/update", user.updateHealthDetails);
 
-    router.post("/addMember", user.addMember);
-    
-     router.get("/", user.findAll);
+    router.post("/addMember",validateToken,user.addMember);
+    //router.get('/data', validateToken, user.findAll);
    
     app.use('/api/users', router);
   };
